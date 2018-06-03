54fd3e700dcd251ca053bf4819e04965b81f5198
wang <358009775@qq.com>
2018-06-04 00:54:00 +0800

    改写成jquery插件

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
+(function ($) {
 
-var items = document.querySelectorAll('.menuItem');
+    $.fn.circleNav = function (option) {
+        var html = '',
+            self = this;
 
-for (var i = 0, len = items.length; i < len; i++) {
-    var item = items[i];
-    item.style.left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / len) * i * Math.PI)).toFixed(4) + "%";
+        // navList
+        if($.isArray(option.navList)) {
+            html += '<div class="ring">';
 
-    item.style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / len) * i * Math.PI)).toFixed(4) + "%";
-}
+            $.each(option.navList, function (i, item) {
+                var left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / option.navList.length) * i * Math.PI)).toFixed(4) + "%",
+                    top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / option.navList.length) * i * Math.PI)).toFixed(4) + "%";
+                
+                html += '<a href="' + (item.href || 'javascript:void(0);') + '" class="menuItem fa ' + item.iconClass + ' fa-2x" style="left: ' + left + '; top: ' + top + ';"></a>';
+            });
 
-document.querySelector('.center').onclick = function (e) {
-    e.preventDefault();
-    var circleDom = document.querySelector('.circle');
-    circleDom.classList.toggle('open');
-}
+            html += '</div>';
+        }
+
+        // 中间的按钮
+        if (option.center  && option.center.iconClass) {
+            html += '<a href="javascript:void(0);" class="center fa ' + option.center.iconClass + ' fa-2x"></a>';
+        }
+
+        // 生成DOM
+        self.html(html);
+
+        // 绑定事件
+        self.off('click.circleNav').on('click.circleNav', '.center', function (e) {
+            e.preventDefault();
+            self.toggleClass('open');
+        });
+
+        return self;
+    };
+})(jQuery);
